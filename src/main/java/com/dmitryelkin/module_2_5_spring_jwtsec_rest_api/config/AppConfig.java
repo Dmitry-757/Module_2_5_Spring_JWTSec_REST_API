bb4cb872f3d9f8ddf480ed71a8a930794cf6f38d
Dmitry-757 <glv11.glv11.glv11@gmail.com>
2023-04-19 16:39:55 +0300

    commit

---
+++ src/main/java/com/dmitryelkin/module_2_5_spring_jwtsec_rest_api/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.dmitryelkin.module_2_5_spring_jwtsec_rest_api.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.dmitryelkin.module_2_5_spring_jwtsec_rest_api")
+public class AppConfig {
+}
---
+++ src/main/java/com/dmitryelkin/module_2_5_spring_jwtsec_rest_api/controller/UserControllerV1.java
@@ -0,0 +1,13 @@
+package com.dmitryelkin.module_2_5_spring_jwtsec_rest_api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/users/*" )
+public class UserControllerV1 {
+
+
+
+
+}
