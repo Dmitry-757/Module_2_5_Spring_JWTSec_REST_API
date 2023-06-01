cc01c51cecb1733fe34cd1ea380b42daa46b4d18
Dmitry757 <43566694+Dmitry757@users.noreply.github.com>
2023-06-02 00:07:35 +0300

    try to write MockMvc-integration test ((

--- src/test/java/com/dmitryelkin/module_2_5_spring_jwtsec_rest_api/controller/UserControllerV1Test.java
+++ src/test/java/com/dmitryelkin/module_2_5_spring_jwtsec_rest_api/controller/UserControllerV1Test.java
@@ -1,40 +1,79 @@
 package com.dmitryelkin.module_2_5_spring_jwtsec_rest_api.controller;
 
+
 import com.dmitryelkin.module_2_5_spring_jwtsec_rest_api.DTO.UserDTO;
 import com.dmitryelkin.module_2_5_spring_jwtsec_rest_api.model.User;
 import com.dmitryelkin.module_2_5_spring_jwtsec_rest_api.repository.UserRepositoryI;
 import com.dmitryelkin.module_2_5_spring_jwtsec_rest_api.service.UserServiceImpl;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
-@ExtendWith(MockitoExtension.class)
+//@ExtendWith(MockitoExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class UserControllerV1Test {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    UserRepositoryI repository;
+
 //    @Mock
 //    UserRepositoryI repository;
 //    @InjectMocks
 //    UserServiceImpl service;
 
-    UserRepositoryI repository = Mockito.mock(UserRepositoryI.class);
     UserServiceImpl service = new UserServiceImpl(repository);
-
-
-
-//    @Mock
-//    UserServiceImpl service;
-//
     UserControllerV1 controller = new UserControllerV1(service);
 
+    @BeforeEach
+    void setup(){
+        repository.deleteAll();
+    }
+
+    @Test
+    public void getAllUsers_whenMockMVC_thenVerifyResponse() throws Exception {
+
+        var users = List.of(
+                new User("user1Name","123"),
+                new User("user2Name","321"),
+                new User("user3Name","213")
+        );
+
+        repository.saveAll(users);
+
+//        ResultActions response = mockMvc.perform(get("/api/employees"));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/users/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$[0].name", Matchers.is("user1Name")));
+//                .andExpect(jsonPath("$[0].cartItems.length()", Matchers.is(1)))
+//                .andExpect(jsonPath("$[0].cartItems[0].item.name", Matchers.is("MacBook")))
+    }
 
     @Test
     void getAllUsers_ReturnsValidResponseEntity() {
+
+        UserRepositoryI repository2 = Mockito.mock(UserRepositoryI.class);
+        UserServiceImpl service = new UserServiceImpl(repository2);
+        UserControllerV1 controller = new UserControllerV1(service);
+
         // given
         var users = List.of(
                 new User("user1Name","123"),
@@ -45,10 +84,10 @@ class UserControllerV1Test {
                 .map(u->(new UserDTO(u.getName())))
                 .toList();
 //        Mockito.doReturn(users).when(this.service).getAll();
-        Mockito.doReturn(users).when(this.repository).findAll();
+        Mockito.doReturn(users).when(repository2).findAll();
 
         // when
-        var responseEntity = this.controller.getAll();
+        var responseEntity = controller.getAll();
 
         // then
         assertNotNull(responseEntity);
