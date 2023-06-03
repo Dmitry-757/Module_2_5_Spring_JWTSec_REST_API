a791e83e25994a4d3e7e633d191b9a86ae0ae816
Dmitry757 <glv11@mail.ru>
2023-06-04 01:51:49 +0300

    add @Repository annotation

--- src/main/java/com/dmitryelkin/module_2_5_spring_jwtsec_rest_api/repository/EventRepositoryI.java
+++ src/main/java/com/dmitryelkin/module_2_5_spring_jwtsec_rest_api/repository/EventRepositoryI.java
@@ -3,9 +3,11 @@ package com.dmitryelkin.module_2_5_spring_jwtsec_rest_api.repository;
 import com.dmitryelkin.module_2_5_spring_jwtsec_rest_api.model.Event;
 import com.dmitryelkin.module_2_5_spring_jwtsec_rest_api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface EventRepositoryI extends JpaRepository<Event, Long> {
     Optional<Event> findByUser(User user);
 }
--- src/main/java/com/dmitryelkin/module_2_5_spring_jwtsec_rest_api/repository/UserRepositoryI.java
+++ src/main/java/com/dmitryelkin/module_2_5_spring_jwtsec_rest_api/repository/UserRepositoryI.java
@@ -5,9 +5,11 @@ import com.dmitryelkin.module_2_5_spring_jwtsec_rest_api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepositoryI extends JpaRepository<User, Long> {
     Optional<User> findByName(String name);
 
