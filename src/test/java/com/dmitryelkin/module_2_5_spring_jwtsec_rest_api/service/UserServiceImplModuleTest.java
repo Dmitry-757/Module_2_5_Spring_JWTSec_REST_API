72709259cdbecf3d0400fdcf980aac97e8930e0e
Dmitry757 <43566694+Dmitry757@users.noreply.github.com>
2023-06-09 00:47:45 +0300

    add module test for delete(), getAll(), getById(), getByName()

--- src/test/java/com/dmitryelkin/module_2_5_spring_jwtsec_rest_api/service/UserServiceImplModuleTest.java
+++ src/test/java/com/dmitryelkin/module_2_5_spring_jwtsec_rest_api/service/UserServiceImplModuleTest.java
@@ -4,6 +4,7 @@ import com.dmitryelkin.module_2_5_spring_jwtsec_rest_api.model.Role;
 import com.dmitryelkin.module_2_5_spring_jwtsec_rest_api.model.Status;
 import com.dmitryelkin.module_2_5_spring_jwtsec_rest_api.model.User;
 import com.dmitryelkin.module_2_5_spring_jwtsec_rest_api.repository.UserRepositoryI;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,6 +13,8 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -24,6 +27,11 @@ class UserServiceImplModuleTest {
 
     @InjectMocks
     private UserServiceImpl service;
+
+//    @BeforeEach
+//    void init(){
+//
+//    }
 
     @Test
     void create() {
@@ -40,28 +48,64 @@ class UserServiceImplModuleTest {
         var responseEntity = service.create(newUser1);
 
         // then
-        assertNotNull(responseEntity);
         assertEquals(newUser2, responseEntity);
-
-    }
-
-    @Test
-    void update() {
     }
 
     @Test
     void delete() {
+
+        // given
+        User user1 = new User(1L,"userName1", "pass1", new ArrayList<>(), Status.ACTIVE, Role.USER);
+        User user2 = new User(1L,"userName1", "pass1", new ArrayList<>(), Status.DELETED, Role.USER);
+        Mockito.doReturn(Optional.of(user2)).when(repository).findById(user1.getId());
+
+        // when
+        var responseEntity = service.delete(user1.getId());
+
+        // then
+        assertEquals(user2, responseEntity);
     }
 
     @Test
     void getAll() {
+        // given
+        var users = List.of(
+                new User(1L,"userName1", "pass1", new ArrayList<>(), Status.ACTIVE, Role.USER),
+                new User(2L,"userName2", "pass2", new ArrayList<>(), Status.ACTIVE, Role.USER),
+                new User(3L,"userName3", "pass3", new ArrayList<>(), Status.ACTIVE, Role.USER)
+        );
+        Mockito.doReturn(users).when(repository).findAll();
+
+        // when
+        var responseEntity = service.getAll();
+
+        // then
+        assertEquals(users, responseEntity);
     }
 
     @Test
     void getById() {
+        // given
+        User user = new User(1L,"userName1", "pass1", new ArrayList<>(), Status.ACTIVE, Role.USER);
+        Mockito.doReturn(Optional.of(user)).when(repository).findById(user.getId());
+
+        // when
+        var responseEntity = service.getById(user.getId());
+
+        // then
+        assertEquals(user, responseEntity);
     }
 
     @Test
     void getByName() {
+        // given
+        User user = new User(1L,"userName1", "pass1", new ArrayList<>(), Status.ACTIVE, Role.USER);
+        Mockito.doReturn(Optional.of(user)).when(repository).findByName(user.getName());
+
+        // when
+        var responseEntity = service.getByName(user.getName());
+
+        // then
+        assertEquals(user, responseEntity);
     }
 }
