e9e359ab3bb585f93c3526d61842267159f1bd4f
Dmitry757 <43566694+Dmitry757@users.noreply.github.com>
2023-06-09 00:04:00 +0300

    add module test for UserServiceImpl.create()

--- src/main/java/com/dmitryelkin/module_2_5_spring_jwtsec_rest_api/service/UserServiceImpl.java
+++ src/main/java/com/dmitryelkin/module_2_5_spring_jwtsec_rest_api/service/UserServiceImpl.java
@@ -11,6 +11,7 @@ import java.util.List;
 @Service
 public class UserServiceImpl implements UserServiceI{
 
+
     private final UserRepositoryI repository;
 
     @Autowired
---
+++ src/test/java/com/dmitryelkin/module_2_5_spring_jwtsec_rest_api/service/UserServiceImplModuleTest.java
@@ -0,0 +1,66 @@
+package com.dmitryelkin.module_2_5_spring_jwtsec_rest_api.service;
+
+import com.dmitryelkin.module_2_5_spring_jwtsec_rest_api.model.Role;
+import com.dmitryelkin.module_2_5_spring_jwtsec_rest_api.model.Status;
+import com.dmitryelkin.module_2_5_spring_jwtsec_rest_api.model.User;
+import com.dmitryelkin.module_2_5_spring_jwtsec_rest_api.repository.UserRepositoryI;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceImplModuleTest {
+
+//    @Mock
+//    private UserRepositoryI repository = Mockito.mock(UserRepositoryI.class);
+
+//    @InjectMocks
+//    private final UserServiceImpl service = new UserServiceImpl(repository);
+
+    @Test
+    void create() {
+
+        UserRepositoryI repository = Mockito.mock(UserRepositoryI.class);
+        UserServiceImpl service = new UserServiceImpl(repository);
+
+        // given
+        User newUser1 = new User("userName1", "pass1");
+        User newUser2 = new User(1L,"userName1", "pass1", new ArrayList<>(), Status.ACTIVE, Role.USER);
+        Mockito.doReturn(newUser2).when(repository).saveAndFlush(newUser1);
+
+        // when
+        var responseEntity = service.create(newUser1);
+
+        // then
+        assertNotNull(responseEntity);
+        assertEquals(newUser2, responseEntity);
+
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void getByName() {
+    }
+}
