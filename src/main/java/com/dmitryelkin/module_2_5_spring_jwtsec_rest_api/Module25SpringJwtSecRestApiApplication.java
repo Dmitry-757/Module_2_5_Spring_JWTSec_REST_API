e3541c85b26da2db88a6c60f0ee5388a7ca76f8c
Dmitry-757 <glv11.glv11.glv11@gmail.com>
2023-06-14 16:55:16 +0300

    add logging for security

--- src/main/java/com/dmitryelkin/module_2_5_spring_jwtsec_rest_api/Module25SpringJwtSecRestApiApplication.java
+++ src/main/java/com/dmitryelkin/module_2_5_spring_jwtsec_rest_api/Module25SpringJwtSecRestApiApplication.java
@@ -3,6 +3,7 @@ package com.dmitryelkin.module_2_5_spring_jwtsec_rest_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@EnableWebSecurity(debug = true)
 @SpringBootApplication
 public class Module25SpringJwtSecRestApiApplication {
 
