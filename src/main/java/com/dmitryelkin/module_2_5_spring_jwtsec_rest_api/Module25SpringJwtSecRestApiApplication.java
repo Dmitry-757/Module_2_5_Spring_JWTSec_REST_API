ad4c9af318908dd9a933bfeff3ba35502eb02996
Dmitry757 <43566694+Dmitry757@users.noreply.github.com>
2023-06-15 01:51:09 +0300

    try to test authentication with RestAssured

--- src/main/java/com/dmitryelkin/module_2_5_spring_jwtsec_rest_api/Module25SpringJwtSecRestApiApplication.java
+++ src/main/java/com/dmitryelkin/module_2_5_spring_jwtsec_rest_api/Module25SpringJwtSecRestApiApplication.java
@@ -1,5 +1,8 @@
 package com.dmitryelkin.module_2_5_spring_jwtsec_rest_api;
 
+import com.dmitryelkin.module_2_5_spring_jwtsec_rest_api.DTO.CredentialsDTO;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,8 +10,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Module25SpringJwtSecRestApiApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(Module25SpringJwtSecRestApiApplication.class, args);
+
+        RestAssured.port = 8090;
+
+        CredentialsDTO credentialsDTO = new CredentialsDTO("user_3", "pass345");
+        Response response = RestAssured
+                .given()
+                .contentType("application/json")
+                .and()
+                .body(credentialsDTO)
+                .when()
+                .request("POST", "/api/v1/auth/signIn/")
+                .then()
+                .extract()
+                .response();
+        System.out.println(response);
     }
 
 }
