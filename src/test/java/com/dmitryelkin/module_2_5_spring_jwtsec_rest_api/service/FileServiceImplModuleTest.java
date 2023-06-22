59d9163ebc46ac25ac94985ece7304b94ccf1723
Dmitry757 <glv11@mail.ru>
2023-06-22 18:42:41 +0300

    add FileServiceImplModuleTest

--- src/test/java/com/dmitryelkin/module_2_5_spring_jwtsec_rest_api/service/FileServiceImplModuleTest.java
+++ src/test/java/com/dmitryelkin/module_2_5_spring_jwtsec_rest_api/service/FileServiceImplModuleTest.java
@@ -2,6 +2,7 @@ package com.dmitryelkin.module_2_5_spring_jwtsec_rest_api.service;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -9,6 +10,7 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Value;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,7 +35,28 @@ class FileServiceImplModuleTest {
     void getAll() {
         // given
         List<String> expectedResult = List.of("file1", "file2", "file3");
-        Mockito.doReturn(expectedResult).when(s3client).listObjects(bucketName);
+
+        List<S3ObjectSummary> mockResult = new ArrayList<>();
+        S3ObjectSummary item = new S3ObjectSummary();
+        item.setKey("file1");
+        mockResult.add(item);
+        item = new S3ObjectSummary();
+        item.setKey("file2");
+        mockResult.add(item);
+        item = new S3ObjectSummary();
+        item.setKey("file3");
+        mockResult.add(item);
+
+        ObjectListing listOfObjects = new ObjectListing();
+        try {
+            Field field = listOfObjects.getClass().getDeclaredField("objectSummaries");
+            field.setAccessible(true);
+            field.set(listOfObjects, mockResult);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        // need somehow to fill listOfObjects
+        Mockito.doReturn(listOfObjects).when(s3client).listObjects(bucketName);
 
         // when
         var response = service.getAll();
