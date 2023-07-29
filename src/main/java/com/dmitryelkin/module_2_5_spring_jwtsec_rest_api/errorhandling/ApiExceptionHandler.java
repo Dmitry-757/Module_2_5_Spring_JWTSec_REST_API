3467ac7221705db14a80dd7cd875cfda94443b6f
Dmitry757 <glv11@mail.ru>
2023-07-29 17:26:36 +0300

    add REST API exception handler

--- src/main/java/com/dmitryelkin/module_2_5_spring_jwtsec_rest_api/errorhandling/ApiExceptionHandler.java
+++ src/main/java/com/dmitryelkin/module_2_5_spring_jwtsec_rest_api/errorhandling/ApiExceptionHandler.java
@@ -1,11 +1,21 @@
 package com.dmitryelkin.module_2_5_spring_jwtsec_rest_api.errorhandling;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.nio.file.AccessDeniedException;
+
 @RestControllerAdvice
 public class ApiExceptionHandler {
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<Object> myAccessDeniedException(AccessDeniedException exception){
+        return new ResponseEntity<Object>(
+                "Access denied ", new HttpHeaders(), HttpStatus.FORBIDDEN);
+    }
+
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<Response> myRuntimeException(RuntimeException exception){
         return ResponseEntity
